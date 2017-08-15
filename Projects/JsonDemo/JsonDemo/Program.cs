3d177758c838558cbf7b772aeebd4dfcf14c5e1a
Mark Licata <malicata@microsoft.com>
2017-08-15 14:47:30 -0700

    committing base application

--- Projects/JsonDemo/JsonDemo/Program.cs
+++ Projects/JsonDemo/JsonDemo/Program.cs
@@ -1,60 +1,53 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
 using System.IO;
-using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
-using System.Collections.Generic;
-using StockManager.Models;
-using Microsoft.Extensions.Logging;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
-using Microsoft.AspNetCore.Hosting;
-using Newtonsoft.Json;
 
 
-namespace StockManager.Controllers
+
+namespace JsonDemo
 {
-    public class QuoteController : Controller
+    public class Program
     {
-        private ILogger<QuoteController> _logger;
-        public IConfigurationRoot Configuration { get; }
-
-
-        public QuoteController(ILogger<QuoteController> logger, IHostingEnvironment env)
-        {
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(env.ContentRootPath)
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
-                .AddEnvironmentVariables();
-            Configuration = builder.Build();
-            _logger = logger;
-        }
-
-
-        [Route("api/testquote")]
-        public IActionResult GetTestQuote()
+        static void Main(string[] args)
         {
             List<string> portfolioSymbols = new List<string>();
-            List<string> comparisonSymbols = new List<string>();
-            List<Quote> myPortfolioData = new List<Quote>();
-            List<Quote> comparisonData = new List<Quote>();
-
             portfolioSymbols = getSymbols($"portfolioSymbols");
 
+            List<string> comparisonSymbols = new List<string>();
             comparisonSymbols = getSymbols($"comparisonSymbols");
 
-            myPortfolioData = loadQuoteData(portfolioSymbols);
-            comparisonData = loadQuoteData(portfolioSymbols);
 
-            return Ok(myPortfolioData.Count + " " + comparisonData.Count);
+
+
+            Console.ReadKey();
+
+
+
+
+            //////TBD: STORE SYMBOLS AND VALUES IN CONFIG
+
+
+            //List<Quote> myPortfolioData = new List<Quote>();
+            //List<Quote> comparisonData = new List<Quote>();
+
+            //myPortfolioData = loadQuoteData(portfolioSymbols);
+            ////comparisonData = loadQuoteData(portfolioSymbols);
+
+            //return Ok(myPortfolioData.Count);
+
+            //List <string> comarisonSymbols = new List<string>();
+
+
         }
 
         private static List<string> getSymbols(string str)
         {
             List<string> temp = new List<string>();
-            string filename = $"appsettings.json";
-            StreamReader file = System.IO.File.OpenText(filename);
+            string filename = $"C:\\Users\\malicata\\Documents\\Visual Studio 2017\\Projects\\StockManager.app\\StockManager\\appsettings.json";
+            StreamReader file = File.OpenText(filename);
             string jsonStr = file.ReadToEnd();
             dynamic jsonObject = JsonConvert.DeserializeObject(jsonStr);
             foreach (var obj in jsonObject[str])
@@ -64,6 +57,7 @@ namespace StockManager.Controllers
             return temp;
         }
 
+        
         private List<Quote> loadQuoteData(List<string> symbols)
         {
             List<Quote> quotesToReturn = new List<Quote>();
@@ -71,30 +65,24 @@ namespace StockManager.Controllers
             foreach (var symbol in symbols)
             {
                 string result = GetHistoricalQuote(symbol, key);
-                if(result != null)
+                if (result != null)
                 {
                     Quote qte = new Quote();
                     dynamic jsonObject = JsonConvert.DeserializeObject(result);
                     qte.symbol = jsonObject["Meta Data"]["2. Symbol"];
-                    int counter = 1;
                     foreach (dynamic obj in jsonObject["Time Series (Daily)"].Children())
                     {
                         Price tempPrice = new Price();
                         tempPrice.date = (DateTime)obj.Name;
                         tempPrice.price = obj.First["4. close"];
                         qte.Prices.Add(tempPrice);
-                        if(counter >=30 )
-                        {
-                            break;
-                        }else{
-                            counter++;
-                        }
                     }
                     quotesToReturn.Add(qte);
                 }
             }
             return quotesToReturn;
         }
+        
 
         public string GetHistoricalQuote(string symbol, string key)
         {
@@ -105,13 +93,14 @@ namespace StockManager.Controllers
                 client.BaseAddress = new Uri(url);
                 client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
                 HttpResponseMessage response = client.GetAsync("").Result;
-                return response.Content.ReadAsStringAsync().Result;
+                return response.Content.ReadAsStringAsync().Result
             }
             catch (Exception e)
             {
-                _logger.LogWarning($"Quote retrieveal failed for {symbol}");
+                //_logger.LogWarning($"Quote retrieveal failed for {symbol}");
                 return null;
             }
         }
+
     }
 }
